ec01d33683d808640a3d9ff6e6cf35f307b98c47
Afgan-0601 <shahguliyev@gmail.com>
2020-12-19 17:13:19 -0500

    Second commit

--- KarateAPITesting/src/test/java/JunitRunner.java
+++ KarateAPITesting/src/test/java/JunitRunner.java
@@ -1,7 +1,6 @@
 
 import org.junit.runner.RunWith;
 
-
 import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
 
--- KarateAPITesting/src/test/java/karate-config.js
+++ KarateAPITesting/src/test/java/karate-config.js
@@ -3,20 +3,20 @@ function fn() {
 // Printing out the env that is 
  karate.log('karate.env system property was:', env);
  if (!env) {
-  env = ‘dev’;
+  env = 'dev';
  }
  var config = {
-	apiURL: ‘https://conduit.productionready.io/api/’
+	apiURL: 'https://conduit.productionready.io/api/'
  }
  if (env == 'dev') {
   config.userEmail = 'mgolovch@gmail.com'
-  config.userPassword = ‘adminpassword’
+  config.userPassword = 'adminpassword'
  } 
  else if (env == 'qa') {
   config.userEmail = 'mgolovch@gmail.com'
-  config.userPassword = ‘adminpassword’
+  config.userPassword = 'adminpassword'
  }
 var accessToken = karate.callSingle('classpath:helpers/CreateToken.feature', config).authToken
-karate.configure(‘headers’, {Authorization: 'Token '+ accessToken})
+karate.configure('headers', {Authorization: 'Token '+ accessToken})
  return config;
 }
